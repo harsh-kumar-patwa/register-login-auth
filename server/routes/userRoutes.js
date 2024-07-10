b7b974af943e07b340c3b005bd2134ccf16b8e43
Harsh Kumar <harshkumar3446@gmail.com>
2024-07-10 11:17:14 +0530

    added register method

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
     try {
-        
+        const userExists = await User.findOne({email: req.body.email});
+        if(userExists) {
+            return res.json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
         const newUser = new User(req.body);
         await newUser.save();
+        return res.status(201).json({ newUser });
 
     } catch (error) {
-        return res.status(400).json({ error });
+        return res.json({ error });
     }
 });
 
 router.post("/login", async (req, res) => {
-  
+    
 });
 
 
